b499841374d86991bdfb384574b4c106d164b818
FebriTahta <febrirtah@gmail.com>
2021-09-24 14:19:21 +0700

    first commit

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -7,9 +7,8 @@
 <link href="{{ asset('tilawatipusat/landing/css/bootstrap.css') }}" rel="stylesheet">
 <link href="{{ asset('tilawatipusat/landing/css/style.css') }}" rel="stylesheet">
 <link href="{{ asset('tilawatipusat/landing/css/responsive.css') }}" rel="stylesheet">
-<meta property="og:title" content="Download E-Certificate"/>
-<meta property="og:description" content="Selamat datang para pecinta Al-Qur'an, terimakasih telah ikut serta
-     dalam diklat tilawati. download e-certificate anda disini"/>
+<meta property="og:title" content="Peserta Baru Pendaftaran Diklat"/>
+<meta property="og:description" content="Konfirmasi data peserta baru"/>
 <meta property="og:image" itemprop="image" content="{{ asset('assets/images/tumb.jpeg') }}">
 @yield('head')
 
