7528f7e522bc89349d3ac76b1519b49427a8c4c8
FebriTahta <febrirtah@gmail.com>
2021-09-24 13:29:14 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+use App\Http\Controllers\KonfirmasiCont;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    // return view('welcome');
+    return redirect('/');
+});
+
+Route::get('/',[KonfirmasiCont::class, 'index'])->name('index.e_confirm');
+Route::get('/data-peserta',[KonfirmasiCont::class,'data_peserta_diklat_menunggu_konfirmasi'])->name('data_peserta');
+Route::post('/konfirmasi-data-peserta-acc',[KonfirmasiCont::class,'acc'])->name('acc');
+
+
+//popok bawang
+Route::get('/2',[KonfirmasiCont::class, 'index2'])->name('seluruh.e_sertifikat');
+
+
