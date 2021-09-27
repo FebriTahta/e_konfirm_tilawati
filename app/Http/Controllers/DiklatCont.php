d71a0736f052574fbaa2f30aa6e46dbf3f9777b6
FebriTahta <febrirtah@gmail.com>
2021-09-27 13:59:21 +0700

    seluruh diklat

--- app/Http/Controllers/DiklatCont.php
+++ app/Http/Controllers/DiklatCont.php
@@ -31,7 +31,7 @@ class DiklatCont extends Controller
                         ->addColumn('program', function($data){
                             return $data->program->name;
                         })
-                        ->rawColumns(['cabang'])
+                        ->rawColumns(['cabang','program'])
                         ->make(true);
             // if(!empty($request->dari))
             // {
