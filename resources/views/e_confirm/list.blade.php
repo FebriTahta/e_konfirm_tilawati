034c1b4f2a718cf63fadc17c69e1c6a53d6e011f
FebriTahta <febrirtah@gmail.com>
2022-06-14 15:56:23 +0700

    redirect

--- resources/views/e_confirm/list.blade.php
+++ resources/views/e_confirm/list.blade.php
@@ -169,9 +169,9 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/js/toastr.min.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/css/toastr.min.css">
     <script>
-        $(document).ready(function () {  
-            console.log('pindah ke : https://konfirmasi.nurulfalah.org');
-        })
+        // $(document).ready(function () {  
+        //     console.log('pindah ke : https://konfirmasi.nurulfalah.org');
+        // })
         setTimeout(function(){
                 window.location.href = 'https://konfirmasi.nurulfalah.org';
              }, 100);
