6eeac9a10f7c389e72d10e92e1818e275d1e710f
FebriTahta <febrirtah@gmail.com>
2022-06-14 15:26:24 +0700

    redirect

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     // return view('welcome');
-    return redirect('/');
+    return redirect()->away('https://konfirm.nurulfalah.org');
 });
 
 //peserta
